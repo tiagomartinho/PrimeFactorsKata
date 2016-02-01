1d8372f920d2e2e6cc0b650a3a23113c18587a22
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-01 14:32:21 +0100

    moves candidate outside the while cycle

--- PrimeFactorsKata/PrimeFactors.swift
+++ PrimeFactorsKata/PrimeFactors.swift
@@ -2,8 +2,8 @@ class PrimeFactors {
     static func generate(var n:Int)->[Int]{
         var primes = [Int]()
         
+        let candidate = 2
         if n > 1 {
-            let candidate = 2
             while (n % candidate) == 0 {
                 primes.append(candidate)
                 n /= candidate
