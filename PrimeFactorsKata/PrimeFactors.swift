0ffc45d7ddc411db5fdf13de97dd09b338a5bac3
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-01 14:24:38 +0100

    passes third test

--- PrimeFactorsKata/PrimeFactors.swift
+++ PrimeFactorsKata/PrimeFactors.swift
@@ -2,7 +2,7 @@ class PrimeFactors {
     static func generate(n:Int)->[Int]{
         var primes = [Int]()
         if n > 1 {
-            primes.append(2)
+            primes.append(n)
         }
         return primes
     }
