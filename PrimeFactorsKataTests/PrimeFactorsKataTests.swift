dcea3fc1f72252e6bf042f40ca7dca0e89418b13
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-01 14:22:18 +0100

    adds second test, generate with 2 should return [2]

--- PrimeFactorsKataTests/PrimeFactorsKataTests.swift
+++ PrimeFactorsKataTests/PrimeFactorsKataTests.swift
@@ -3,9 +3,19 @@ import XCTest
 
 class PrimeFactorsKataTests: XCTestCase {
     
-    let list = [Int]()
+    func list(ints:Int...)->[Int]{
+        var list = [Int]()
+        for i in ints {
+            list.append(i)
+        }
+        return list
+    }
     
     func testOne() {
-        XCTAssertEqual(list, PrimeFactors.generate(1))
+        XCTAssertEqual(list(), PrimeFactors.generate(1))
+    }
+    
+    func testTwo() {
+        XCTAssertEqual(list(2), PrimeFactors.generate(2))
     }
 }
