53b167876c730713207ccc9ef8174da2291a8ccb
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-01 14:11:42 +0100

    Initial Commit

---
+++ PrimeFactorsKata/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PrimeFactorsKata
+//
+//  Created by Tiago Martinho on 01/02/16.
+//  Copyright © 2016 TM. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
