e5bffa1dd5ad961c55650a3db1e670ea8ddb6bcb
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-01 14:33:08 +0100

    does while loop while incrementing the candidate

--- PrimeFactorsKata/PrimeFactors.swift
+++ PrimeFactorsKata/PrimeFactors.swift
@@ -2,12 +2,13 @@ class PrimeFactors {
     static func generate(var n:Int)->[Int]{
         var primes = [Int]()
         
-        let candidate = 2
-        if n > 1 {
+        var candidate = 2
+        while n > 1 {
             while (n % candidate) == 0 {
                 primes.append(candidate)
                 n /= candidate
             }
+            candidate += 1
         }
         
         if n > 1 {
