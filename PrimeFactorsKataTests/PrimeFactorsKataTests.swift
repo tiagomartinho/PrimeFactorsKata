37f50e1893aa6a96c7b0344dd67a594ba50c7fb5
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-02 14:08:19 +0100

    renames tests, adds test for four

--- PrimeFactorsKataTests/PrimeFactorsKataTests.swift
+++ PrimeFactorsKataTests/PrimeFactorsKataTests.swift
@@ -5,15 +5,19 @@ class PrimeFactorsKataTests: XCTestCase {
     
     let emptyList = [Int]()
     
-    func testOneGeneratesEmptyList() {
+    func testOne() {
         XCTAssertEqual(emptyList, PrimeFactors.generate(1))
     }
     
-    func testTwoGeneratesTwo() {
+    func testTwo() {
         XCTAssertEqual([2], PrimeFactors.generate(2))
     }
     
-    func testThreeGeneratesThree() {
+    func testThree() {
         XCTAssertEqual([3], PrimeFactors.generate(3))
+    }
+    
+    func testFour() {
+        XCTAssertEqual([2,2], PrimeFactors.generate(4))
     }
 }
