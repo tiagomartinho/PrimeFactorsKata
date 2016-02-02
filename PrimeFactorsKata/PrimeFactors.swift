461ac6f2ba724d94bd78227ffd8ff15f60544f5f
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-02 14:24:29 +0100

    manages eight case

--- PrimeFactorsKata/PrimeFactors.swift
+++ PrimeFactorsKata/PrimeFactors.swift
@@ -4,11 +4,9 @@ class PrimeFactors {
         var primes = [Int]()
         var primeFactor = n
         
-        if primeFactor % 2 == 0 {
-            if primeFactor / 2 > 1 {
-                primeFactor = primeFactor / 2
-                primes.append(2)
-            }
+        while primeFactor % 2 == 0 {
+            primeFactor = primeFactor / 2
+            primes.append(2)
         }
         
         if primeFactor > 1 {
