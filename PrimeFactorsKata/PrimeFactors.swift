975fa427fee47f144fd8df9d6f9e51d4c6a5ed5c
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-03 14:19:53 +0100

    replace while with for loop

--- PrimeFactorsKata/PrimeFactors.swift
+++ PrimeFactorsKata/PrimeFactors.swift
@@ -2,14 +2,12 @@ class PrimeFactors {
     static func generate(n:Int)->[Int]{
         var primes = [Int]()
         var remaining = n
-        var divisor = 2
         
-        while divisor <= n {
+        for var divisor=2; divisor <= n; divisor+=1 {
             while remaining % divisor == 0 {
                 remaining /= divisor
                 primes.append(divisor)
             }
-            divisor += 1
         }
         
         return primes
