28bee23831309b570d1a8fa39b8150651e083364
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-03 14:05:18 +0100

    adds control that numbers divisible by two in generate algorithm

--- PrimeFactorsKata/PrimeFactors.swift
+++ PrimeFactorsKata/PrimeFactors.swift
@@ -3,9 +3,11 @@ class PrimeFactors {
         var primes = [Int]()
         var remaining = n
         
-        if n / 2 > 1 {
-            remaining /= 2
-            primes.append(2)
+        if n % 2 == 0 {
+            if n / 2 > 1 {
+                remaining /= 2
+                primes.append(2)
+            }
         }
         
         if n > 1 {
