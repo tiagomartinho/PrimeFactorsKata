7f3e0e6ffba3083ac74d59c09c476122514f44f4
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-03 14:17:18 +0100

    includes if in while loop

--- PrimeFactorsKata/PrimeFactors.swift
+++ PrimeFactorsKata/PrimeFactors.swift
@@ -4,16 +4,12 @@ class PrimeFactors {
         var remaining = n
         var divisor = 2
         
-        while divisor < n {
+        while divisor <= n {
             while remaining % divisor == 0 {
                 remaining /= divisor
                 primes.append(divisor)
             }
             divisor += 1
-        }
-        
-        if remaining > 1 {
-            primes.append(remaining)
         }
         
         return primes
