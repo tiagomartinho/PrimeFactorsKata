0f61329fab8cd78d7cab0d5ee9df3e4755745a2b
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-05 14:09:20 +0100

    divides n by two to find prime numbers

--- PrimeFactorsKata/PrimeFactors.swift
+++ PrimeFactorsKata/PrimeFactors.swift
@@ -1,6 +1,10 @@
 class PrimeFactors {
-    static func generate(n:Int)->[Int]{
+    static func generate(var n:Int)->[Int]{
         var primes = [Int]()
+        if n % 2 == 0 {
+            n /= 2
+            primes.append(2)
+        }
         if n > 1 {
             primes.append(n)
         }
