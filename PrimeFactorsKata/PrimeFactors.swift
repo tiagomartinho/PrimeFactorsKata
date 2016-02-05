f5bcadafc4798d1d1b58a2764853a8024e624317
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-05 14:12:41 +0100

    extracts divisor constant

--- PrimeFactorsKata/PrimeFactors.swift
+++ PrimeFactorsKata/PrimeFactors.swift
@@ -1,9 +1,10 @@
 class PrimeFactors {
     static func generate(var n:Int)->[Int]{
         var primes = [Int]()
-        while n % 2 == 0 {
-            n /= 2
-            primes.append(2)
+        let divisor = 2
+        while n % divisor == 0 {
+            n /= divisor
+            primes.append(divisor)
         }
         if n > 1 {
             primes.append(n)
