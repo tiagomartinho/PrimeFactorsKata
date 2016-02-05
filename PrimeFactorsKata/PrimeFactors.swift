9023d73b247b5513a4255af360cab10fe99a25f0
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-05 14:13:42 +0100

    loops incrementing divisor to find primes

--- PrimeFactorsKata/PrimeFactors.swift
+++ PrimeFactorsKata/PrimeFactors.swift
@@ -1,10 +1,13 @@
 class PrimeFactors {
     static func generate(var n:Int)->[Int]{
         var primes = [Int]()
-        let divisor = 2
-        while n % divisor == 0 {
-            n /= divisor
-            primes.append(divisor)
+        var divisor = 2
+        while divisor <= n {
+            while n % divisor == 0 {
+                n /= divisor
+                primes.append(divisor)
+            }
+            divisor += 1
         }
         if n > 1 {
             primes.append(n)
